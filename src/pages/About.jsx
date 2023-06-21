7b5acc2c412c7b03f63db336d3e27ca02e385f20
paolaguerra <paolaguerraph@gmail.com>
2023-06-21 13:26:44 -0400

    About - CV

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,236 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div class="bg-white py-5 sm:py-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto max-w-1xl lg:text-center">
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+              About me
+            </p>
+            <p class="mt-6 text-sm leading-8 text-gray-600">
+              Having worked as a designer for several years, initially
+              specializing in graphic design and later transitioning into UX
+              design, I embarked on a new adventure four years ago when my
+              husband and I relocated to the United States. Throughout this
+              time, my visa (H4) restricted my ability to work, prompting me to
+              channel my energy into the software development world. <br />
+              <br />
+              Over the past couple of years, I've studied ReactJS, an incredible
+              library that has opened up boundless opportunities for me to
+              create innovative solutions. I've improved my skills by building
+              diverse projects from scratch, ranging from a number calculator to
+              a notes application. While momentarily diverting my attention from
+              design, my current focus revolves around further deepening my
+              expertise in React and embracing progressive growth through
+              collaboration with seasoned developers. <br />
+              <br />
+              With an unwavering commitment to continuous improvement, I eagerly
+              seek opportunities to learn from and collaborate with industry
+              experts. My passion lies in pushing boundaries, solving complex
+              problems, and contributing to remarkable projects alongside
+              talented developers. I am excited to embark on this journey of
+              professional growth and forge meaningful connections within the
+              software development community.
+            </p>
+          </div>
+          <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-12 lg:max-w-4xl">
+            <p class="mt-2 mb-12 text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl text-center">
+              Technical Skills
+            </p>
+          </div>
+          <div className="flex flex-row justify-center">
+            <span class="mr-5 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+              React JS
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+              Javascript
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+              HTML
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+              CSS
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              Bootstrap
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
+              Tailwind
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
+              MUI
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">
+              Animate.css
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+              Adobe Photoshop
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+              Adobe Illustrator
+            </span>
+            <span class="mr-5 inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              Adobe XD
+            </span>
+          </div>
+
+          <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-12 lg:max-w-4xl">
+            <p class="mt-2 mb-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl text-center">
+              Experience
+            </p>
+            <ul class="divide-y divide-gray-100">
+              <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      Nimble Innovation Ltd
+                    </p>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                      I worked at Nimble, with a team of UX designers, were we
+                      created a crypto-wallet aiming to develop an <br />
+                      economic infrastructure tackling global issues, and
+                      providing as yet unseen benefits to all crypto
+                      enthusiasts.
+                    </p>
+                  </div>
+                </div>
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                  <p class="text-sm leading-6 text-gray-900">UX Designer</p>
+                  <p class="mt-1 text-xs leading-5 text-gray-500">
+                    2018 - 2019
+                  </p>
+                </div>
+              </li>
+              <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      Workast
+                    </p>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                      I worked remotely for Workast, a start up located in
+                      California. I worked on the product re design, a<br /> web
+                      app to create a list of tasks and manage team's workflows.
+                      I also worked on the product identity. This was a <br />
+                      short project for two months.
+                    </p>
+                  </div>
+                </div>
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                  <p class="text-sm leading-6 text-gray-900">UX Designer</p>
+                  <p class="mt-1 text-xs leading-5 text-gray-500">2017-2017</p>
+                </div>
+              </li>
+              <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      Wunderman Buenos Aires
+                    </p>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                      I worked as a Content Manager and among my
+                      responsabilities were basic technical knowledge of HTML{" "}
+                      <br /> and web publishing, familiarity with social media,
+                      create and publish engaging content and basic database
+                      management.
+                    </p>
+                  </div>
+                </div>
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                  <p class="text-sm leading-6 text-gray-900">Content Manager</p>
+                  <p class="mt-1 text-xs leading-5 text-gray-500">2016-2017</p>
+                </div>
+              </li>
+              <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      Creative Works
+                    </p>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                      I worked remotely for Creative Works, a company based in
+                      New York, my main task was UI Design, <br />
+                      and the creation of responsive websites.
+                    </p>
+                  </div>
+                </div>
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                  <p class="text-sm leading-6 text-gray-900">
+                    Web and Graphic Designer
+                  </p>
+                  <p class="mt-1 text-xs leading-5 text-gray-500">2015-2016</p>
+                </div>
+              </li>
+              <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex gap-x-4">
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-sm font-semibold leading-6 text-gray-900">
+                      Graphic Designer
+                    </p>
+                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+                      I started here as a Junior Graphic Desiger, I was in
+                      charge of the creation of things like baners, <br />
+                      email design, some basic work with HTML and CSS among
+                      other things. I also worked with CMS Magento <br />
+                      for some important e-commerce the company has.
+                    </p>
+                  </div>
+                </div>
+                <div class="hidden sm:flex sm:flex-col sm:items-end">
+                  <p class="text-sm leading-6 text-gray-900">
+                    Ulula Information Technology
+                  </p>
+                  <p class="mt-1 text-xs leading-5 text-gray-500">2014-2015</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl text-center">
+              Education
+            </p>
+          </div>
+          <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-10 lg:max-w-4xl">
+            <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+              <div class="relative pl-16">
+                <dt class="text-sm font-semibold leading-7 text-gray-900">
+                  UX Design
+                </dt>
+                <dd class="mt-2 text-sm leading-7 text-gray-600">
+                  Digital House / Buenos Aires - Argentina
+                </dd>
+              </div>
+              <div class="relative pl-16">
+                <dt class="text-sm font-semibold leading-7 text-gray-900">
+                  Web Design UI/UX
+                </dt>
+                <dd class="mt-2 text-sm leading-7 text-gray-600">
+                  Espacio Virgen / Buenos Aires - Argentina
+                </dd>
+              </div>
+              <div class="relative pl-16">
+                <dt class="text-sm font-semibold leading-7 text-gray-900">
+                  Professional Photography
+                </dt>
+                <dd class="mt-2 text-base leading-7 text-gray-600">
+                  ISEC / Buenos Aires - Argentina
+                </dd>
+              </div>
+              <div class="relative pl-16">
+                <dt class="text-sm font-semibold leading-7 text-gray-900">
+                  Social Communication
+                </dt>
+                <dd class="mt-2 text-sm leading-7 text-gray-600">
+                  Universidad Rafael Belloso Chacín / Maracaibo - Venezuela
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
